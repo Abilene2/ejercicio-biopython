d598af0a57f0bbd8e9309b942fc1270fec6e09fc
aby <abilenebeltran@gmail.com>
2020-10-14 14:58:03 -0700

    Actualizacion del codigo del script

--- script.py
+++ script.py
@@ -1,20 +1,22 @@
-from Bio import Seq, SeqIO, SeqFeature
+from Bio import SeqIO, SeqFeature
 from Bio.SeqFeature import SeqFeature, FeatureLocation
+import os
+
+nomArchivo = "ls_orchid.gbk"
 
 def summarize_contents(nombre_archivo):
     print("name: ", nombre_archivo)
-    print('path: ...')
-
+    print('path: ', os.path.dirname(nombre_archivo))
 
     records = list(SeqIO.parse(nombre_archivo,"genbank"))
     print("num_records", len(records))
 
-    print("Records")
-    if len(records) == 1 :
-        record = SeqIO.read(nombre_archivo, "genbank")
-        print("ID: ", record.id)
+    for seq_record in SeqIO.parse(nombre_archivo, "genbank"):
+        print("name: ", seq_record.name)
+        print("ID: ", seq_record.id)
+        print("location: ")
+        for seq_features in seq_record.features:
+            print("start: %d, stop %d"%(int(seq_features.location.start),int(seq_features.location.end)))
 
-    else:     
-        for seq_record in SeqIO.parse(nombre_archivo, "genbank"):
-            print("ID: ", seq_record.id)
+summarize_contents(nomArchivo)
 
