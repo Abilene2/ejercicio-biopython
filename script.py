301781071e283c0f93e6712365918c963f7a7c40
aby <abilenebeltran@gmail.com>
2020-10-14 18:35:49 -0700

    Modificacion para no usar el objeto SeqFeature

--- script.py
+++ script.py
@@ -1,22 +1,15 @@
-from Bio import SeqIO, SeqFeature
-from Bio.SeqFeature import SeqFeature, FeatureLocation
+from Bio import SeqIO
 import os
-
-nomArchivo = "ls_orchid.gbk"
 
 def summarize_contents(nombre_archivo):
     print("name: ", nombre_archivo)
-    print('path: ', os.path.dirname(nombre_archivo))
+    print('path: ', os.path.abspath(nombre_archivo))
 
     records = list(SeqIO.parse(nombre_archivo,"genbank"))
     print("num_records", len(records))
 
+    print("records: ")
     for seq_record in SeqIO.parse(nombre_archivo, "genbank"):
-        print("name: ", seq_record.name)
-        print("ID: ", seq_record.id)
-        print("location: ")
-        for seq_features in seq_record.features:
-            print("start: %d, stop %d"%(int(seq_features.location.start),int(seq_features.location.end)))
-
-summarize_contents(nomArchivo)
-
+        print(" - id: ", seq_record.id)
+        print("   name: ", seq_record.name)
+        print("   description: ", seq_record.description)
