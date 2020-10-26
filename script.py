da19d6de0332ccde3eb3f62648e370f066106112
aby <abilenebeltran@gmail.com>
2020-10-25 19:04:39 -0700

    Se modifico la funcion summarize_contents para regresar una cadena en lugar de imprimir el resultado en consola

--- script.py
+++ script.py
@@ -2,14 +2,20 @@ from Bio import SeqIO
 import os
 
 def summarize_contents(nombre_archivo):
-    print("name: ", nombre_archivo)
-    print('path: ', os.path.abspath(nombre_archivo))
+    resumen = "name: " + nombre_archivo
+
+    resumen += "\npath: " + os.path.abspath(nombre_archivo)
 
     records = list(SeqIO.parse(nombre_archivo,"genbank"))
-    print("num_records", len(records))
+    
+    resumen += "\nnum_records: " + str(len(records))
 
-    print("records: ")
+    resumen += "\nrecords: "
     for seq_record in SeqIO.parse(nombre_archivo, "genbank"):
-        print(" - id: ", seq_record.id)
-        print("   name: ", seq_record.name)
-        print("   description: ", seq_record.description)
+        resumen += "\n - id: " + seq_record.id
+
+        resumen += "\n   name: " + seq_record.name
+
+        resumen += "\n   description: " + seq_record.description
+
+    return resumen
